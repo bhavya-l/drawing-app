98b1d6adc2a83691636771dce13e86cf6d09578f
Bhavya Lamba <bhavyalamba56@gmail.com>
2025-08-04 20:23:49 -0400

    cursors working (kind of)

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,10 +1,48 @@
-import '../styles/Canvas.css';
+import "../styles/Canvas.css";
+import { useEffect, useRef } from "react";
+import useWebSocket from "react-use-websocket";
+import throttle from "lodash.throttle";
+import { Cursor } from "./Cursor";
 
-const CanvasBoard = () => {
+const renderCursors = (users: any) => {
+  return Object.keys(users).map((userId) => {
+    const user = users[userId];
+    return <Cursor key={userId} point={[user.state.x, user.state.y]}></Cursor>;
+  });
+};
+
+export default function CanvasBoard({ username }: { username: string }) {
+  const WS_URL = "ws://127.0.0.1:8000";
+  const socket = useWebSocket(WS_URL, {
+    queryParams: { username },
+  });
+
+  const THROTTLE = 100;
+  const sendJsonMessageThrottled = useRef(
+    throttle(socket.sendJsonMessage, THROTTLE)
+  );
+
+  useEffect(() => {
+    socket.sendJsonMessage({
+      x: 0,
+      y: 0,
+    });
+    window.addEventListener("mousemove", (e) => {
+      sendJsonMessageThrottled.current({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    });
+  }, []);
+
+  if (socket.lastJsonMessage) {
+    return (
+      <div className="canvas">{renderCursors(socket.lastJsonMessage)}</div>
+    );
+  }
+
   return (
-    <div className="canvas"> 
+    <div className="canvas">
     </div>
   );
 }
-
-export default CanvasBoard;
